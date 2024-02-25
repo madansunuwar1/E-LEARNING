dd0776d2afbf1bcd9308d616b2d6cdeed256134a
Madan sunuwar <madansun7@gmail.com>
2024-02-25 17:33:32 +0545

    "faq design added"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="text-black border-b-2 pb-64 font-Oswald">
-        <div className="h-[30vh] md:h-[60vh] bg-contain flex justify-center mt-12">
+      <div className="text-black border-b-2 font-Oswald flex flex-col py-24 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <div className="h-[30vh] md:h-[60vh] bg-contain flex justify-center">
           <div className="text-6xl md:text-9xl text-white font-bold">
             <div className="relative font-Lora">
               <div className="flex gap-9 text-center">
@@ -21,10 +21,31 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <button className="text-green-400 mt-56 border mx-auto px-5 py-2 text-xl font-semibold hover:bg-white hover:text-black md:hidden">Take test</button>
       </div>
-      <div className="px-4 py-20 flex gap-7 border-b-2">
+      <div className="py-20 flex gap-7 border-b-2 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <h1 className="text-green-400 text-2xl font-[500] text-center font-Oswald">Greetings, Students!</h1>
-        <p className="text-green-400 font-Oswald font-extralight text-md">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque labore porro voluptas. Nostrum, quos numquam tempore non saepe voluptatem voluptatum voluptates explicabo eum fugiat, nihil obcaecati. Voluptate perspiciatis, magnam magni voluptatem animi, deserunt, saepe illum facere sint veniam fuga iure architecto dolore dolorum accusantium esse repudiandae eius iusto? Cumque quis officiis dolorum excepturi ad fugit harum explicabo laudantium reprehenderit temporibus vitae eligendi repellendus dolore quia, quaerat nam magnam incidunt ab molestias accusantium obcaecati eveniet? Obcaecati voluptatibus aliquid animi molestias fuga nobis excepturi.</p>
+        <p className="text-green-400 font-Oswald font-extralight text-md text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque labore porro voluptas. Nostrum, quos numquam tempore non saepe voluptatem voluptatum voluptates explicabo eum fugiat, nihil obcaecati. Voluptate perspiciatis, magnam magni voluptatem animi, deserunt, saepe illum facere sint veniam fuga iure architecto dolore dolorum accusantium esse repudiandae eius iusto? Cumque quis officiis dolorum excepturi ad fugit harum explicabo laudantium reprehenderit temporibus vitae eligendi repellendus dolore quia, quaerat nam magnam incidunt ab molestias accusantium obcaecati eveniet? Obcaecati voluptatibus aliquid animi molestias fuga nobis excepturi.</p>
+      </div>
+      <div className="py-16 gap-7 border-b-2 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <h1 className="text-green-400 text-4xl font-[500] text-center font-Oswald">FAQ</h1>
+        <div className="flex justify-center">
+          <ul className="text-green-200 mt-8 flex flex-col gap-4 list-disc">
+            <li>
+              What is main concern of students ?
+              <p className=" before:'->'">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est sit sint aliquid, tempore suscipit!</p>
+            </li>
+            <li>
+              When will the classes start ?
+            </li>
+            <li>
+              Is it for free ?
+            </li>
+            <li>
+              what kind of courses you offer
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,52 @@
 import Link from 'next/link'
 import React from 'react'
+import{
+  ImFacebook,
+  ImInstagram,ImYoutube
+} from 'react-icons/im'
 
 const Footer = () => {
   return (
     <>
-    <div className='flex justify-evenly px-4 py-10'>
+      <div className='flex justify-evenly py-10 '>
         <div className='flex items-center font-Oswald font-bold text-6xl text-green-200'>
-            LOGO
+          LOGO
         </div>
         <div className='flex flex-col'>
-            <h1 className='mb-4 text-green-400 underline font-Oswald font-semibold text-md'>Links</h1>
-            <div className='flex flex-col text-sm text-green-200'>
-           <Link href={'/'}>Home</Link>
-           <Link href={'/'}>About</Link> 
-           <Link href={'/'}>Contact</Link> 
-           </div>
+          <h1 className='mb-4 text-green-400 underline font-Oswald font-semibold text-md'>Links</h1>
+          <div className='flex flex-col text-sm text-green-200 font-extralight'>
+            <Link href={'/'}>Home</Link>
+            <Link href={'/'}>About</Link>
+            <Link href={'/'}>Contact</Link>
+          </div>
         </div>
         <div className='flex flex-col'>
-            <h1 className='mb-4 text-green-400 underline font-Oswald font-semibold text-md'>Our Services</h1>
-            <div className='flex flex-col text-sm text-green-200 font-extralight'>
-           <Link href={'/'}>Classes</Link>
-           <Link href={'/'}>Consultation</Link> 
-           <Link href={'/'}>Help</Link>
-           </div>
+          <h1 className='mb-4 text-green-400 underline font-Oswald font-semibold text-md'>Our Services</h1>
+          <div className='flex flex-col text-sm text-green-200 font-extralight'>
+            <Link href={'/'}>Classes</Link>
+            <Link href={'/'}>Consultation</Link>
+            <Link href={'/'}>Help</Link>
+          </div>
         </div>
-    </div>
-    <div className='text-center text-green-600 pb-4'>
+      </div>
+      <div className='flex pb-4 justify-evenly'>
+        <div className='text-center text-green-600'>
         copyright &copy; Nikesh
-    </div>
+        </div>
+        <div className='text-green-600'>
+          <ul className='flex gap-x-4'>
+            <li>
+              <a herf="http://www.facebook.com" target='_blank'><ImFacebook /></a>
+            </li>
+            <li>
+              <a herf="http://www.instagram.com" target='_blank'><ImInstagram /></a>
+            </li>
+            <li>
+              <a herf="http://www.youtube.com" target='_blank'><ImYoutube /></a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
