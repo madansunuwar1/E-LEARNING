fb93f702bb64eaea791f4c47921e5e2fb749b69f
Madan sunuwar <madansun7@gmail.com>
2024-02-25 18:01:56 +0545

    "hero section padding adjusted for small devices"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="text-black border-b-2 font-Oswald flex flex-col py-24 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="text-black border-b-2 font-Oswald flex flex-col pb-8 pt-4 md:py-24 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <div className="h-[30vh] md:h-[60vh] bg-contain flex justify-center">
           <div className="text-6xl md:text-9xl text-white font-bold">
             <div className="relative font-Lora">
@@ -25,15 +25,15 @@ export default function Home() {
       </div>
       <div className="py-20 flex gap-7 border-b-2 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <h1 className="text-green-400 text-2xl font-[500] text-center font-Oswald">Greetings, Students!</h1>
-        <p className="text-green-400 font-Oswald font-extralight text-md text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque labore porro voluptas. Nostrum, quos numquam tempore non saepe voluptatem voluptatum voluptates explicabo eum fugiat, nihil obcaecati. Voluptate perspiciatis, magnam magni voluptatem animi, deserunt, saepe illum facere sint veniam fuga iure architecto dolore dolorum accusantium esse repudiandae eius iusto? Cumque quis officiis dolorum excepturi ad fugit harum explicabo laudantium reprehenderit temporibus vitae eligendi repellendus dolore quia, quaerat nam magnam incidunt ab molestias accusantium obcaecati eveniet? Obcaecati voluptatibus aliquid animi molestias fuga nobis excepturi.</p>
+        <p className="text-green-400 font-extralight font-Oswald text-md text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque labore porro voluptas. Nostrum, quos numquam tempore non saepe voluptatem voluptatum voluptates explicabo eum fugiat, nihil obcaecati. Voluptate perspiciatis, magnam magni voluptatem animi, deserunt, saepe illum facere sint veniam fuga iure architecto dolore dolorum accusantium esse repudiandae eius iusto? Cumque quis officiis dolorum excepturi ad fugit harum explicabo laudantium reprehenderit temporibus vitae eligendi repellendus dolore quia, quaerat nam magnam incidunt ab molestias accusantium obcaecati eveniet? Obcaecati voluptatibus aliquid animi molestias fuga nobis excepturi.</p>
       </div>
       <div className="py-16 gap-7 border-b-2 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <h1 className="text-green-400 text-4xl font-[500] text-center font-Oswald">FAQ</h1>
         <div className="flex justify-center">
-          <ul className="text-green-200 mt-8 flex flex-col gap-4 list-disc">
+          <ul className="text-green-400 mt-8 font-extralight flex flex-col gap-4 list-disc font-Oswald text-md text-justify mx-6">
             <li>
               What is main concern of students ?
-              <p className=" before:'->'">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est sit sint aliquid, tempore suscipit!</p>
+              <p className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est sit sint aliquid, tempore suscipit!</p>
             </li>
             <li>
               When will the classes start ?
